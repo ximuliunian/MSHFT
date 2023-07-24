d55348ace45e383cd4566366d430da6ddcefd6c6
曦暮流年 <2515550574@qq.com>
2023-07-24 10:54:14 +0800

    2023年7月24日 上午更新 详情信息更新

---
+++ main/java/About.java
@@ -0,0 +1,20 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class About {
+    @FXML
+    private Label banquan;
+
+    public void initialize() {
+        banquan.setText("Copyright © 曦暮流年 2023 - " + aDate() + ". All Rights Reserved.");
+    }
+
+    private String aDate() {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy");
+        return format.format(new Date());
+    }
+}
